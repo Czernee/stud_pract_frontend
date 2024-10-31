286f1aea389bae6b1a33e3c8192533bcc9139ad3
Daniil Chernousov <i@tehnik-7.ru>
2024-10-31 06:11:10 +0300

    configuring main file done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import { useTelegram } from './components/hooks/useTelegram';
+import { Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
+import { Categories } from './components/Categories/Categories';
 
 function App() {
-  
+  const { tg } = useTelegram();
+  const [activeCategory, setActiveCategory] = useState('Все');
+
+  useEffect(() => {
+    tg.ready();
+  }, [tg]);
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route
+          index
+          element={
+            <>
+              <Categories activeCategory={activeCategory} setActiveCategory={setActiveCategory} />
+              <ProductList activeCategory={activeCategory} />
+            </>
+          }
+        />
+        <Route path={'form'} element={<Form />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
